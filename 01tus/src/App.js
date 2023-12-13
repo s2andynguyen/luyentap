18397b6066f8f65b798d535c88722c0c1526c264
s2andynguyen <thanhtungnguyen9911@gmail.com>
2023-12-13 08:18:35 +0700

    fr1

---
+++ 01tus/src/App.js
@@ -0,0 +1,45 @@
+import Board from './Board'
+import './style/game.css';
+import { useState } from 'react'
+
+
+function App() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return (
+    <div className="App">
+      <div className="game-board">
+        <h1 className='app-title'>TikToe Game</h1>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
